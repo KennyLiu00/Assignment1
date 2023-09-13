7d5866c360b4dd7e9501c3d8f965d2090f2cd191
Kenny Liu <143909151+KennyLiu00@users.noreply.github.com>
2023-09-13 15:52:05 -0800

    Update test1.cpp

--- assign1/test1.cpp
+++ assign1/test1.cpp
@@ -21,8 +21,8 @@ int main(/*int argc, char* argv[]*/) {
     cout << "************Performing Test 1********************" << endl;
     cout << "=================================" << endl;
 
-    std::ReadyQueue q1;
-    std::PCBTable table;
+    MyNamespace::ReadyQueue q1;
+    MyNamespace::PCBTable table;
 
     for (int i = 1; i <= 50; i++) {
         // Add a new PCB with id = i, priority = i to the table at index i
