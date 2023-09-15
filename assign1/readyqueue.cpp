5f01e4bb6927b72f2a6a6fd4824c4ccb0a316198
Kenny Liu <143909151+KennyLiu00@users.noreply.github.com>
2023-09-15 12:42:31 -0700

    DONE

--- assign1/readyqueue.cpp
+++ assign1/readyqueue.cpp
@@ -1,25 +1,24 @@
-#ifndef READYQUEUE_H
-#define READYQUEUE_H
 #include <iostream>
 #include "readyqueue.h"
-#include "pcb.h"
-
 
 using namespace std;
+
+//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient comments to your code
+
 
 /**
  * @brief Constructor for the ReadyQueue class.
  */
- ReadyQueue::ReadyQueue() : Q{} { //Q starts with an empty array
-     count = 0; //initializing the number of elements that will be in the heap
- }
+ ReadyQueue::ReadyQueue(int initialCapacity) : rsize(0), capacity(initialCapacity) {
+    arrqueue = new PCB*[capacity];
+}
 
 /**
  *@brief Destructor
 */
 ReadyQueue::~ReadyQueue() {
-    //TODO: add your code to release dynamically allocate memory
- 
+    delete[] arrqueue;
 }
 
 /**
@@ -28,18 +27,41 @@ ReadyQueue::~ReadyQueue() {
  * @param pcbPtr: the pointer to the PCB to be added
  */
 void ReadyQueue::addPCB(PCB *pcbPtr) {
-  // When adding a PCB to the queue, you must change its state to READY.
-  
-  if(count >= MAX) //if the queue is full
-    return;
+    if (rsize == capacity) {
+        // Double the capacity and reallocate memory for the array
+        capacity *= 2;
+        PCB** newArr = new PCB*[capacity];
+        
+        // Copy elements from the old array to the new array
+        for (int i = 0; i < rsize; i++) {
+            newArr[i] = arrqueue[i];
+        }
+        
+        // Deallocate memory for the old array
+        delete[] arrqueue;
+        
+        // Update arrqueue to point to the new array
+        arrqueue = newArr;
+    }
+    int insertIndex = rsize;
+    // Find the correct position to insert the process based on its priority
+    for (int i = 0; i < rsize; i++) {
+        if (pcbPtr->getPriority() > arrqueue[i]->getPriority()) {
+            insertIndex = i;
+            break;
+        }
+    }
+    // Shift elements to make room for the new process
+    for (int i = rsize; i > insertIndex; i--) {
+        arrqueue[i] = arrqueue[i - 1];
+    }
 
-  else // queue is not full
-  {
-    Q[count] = pcbPtr; // adding pcb into the queue
-    count++; // increment size
-    pcbPtr->setState(ProcState::READY); // state to ready
-    trickleup(); // pcb needs to be swapped until in the right location
-  }
+    // Insert the process at the correct position
+    arrqueue[insertIndex] = pcbPtr;
+    rsize++;
+
+    // Set the state of the inserted process
+    pcbPtr->setState(ProcState::READY);
 }
 
 /**
@@ -48,126 +70,42 @@ void ReadyQueue::addPCB(PCB *pcbPtr) {
  * @return PCB*: the pointer to the PCB with the highest priority
  */
 PCB* ReadyQueue::removePCB() {
-  if(count == 0) //if there is no elements inside of the heap
-  {
-    return NULL; //since it is empty, it will be NULL
-  }
-
-// Reheapifying - after last job is replaced as root, need to trickle down to ensure it is in the right location
-
-  PCB* to_remove = Q[0]; // variable to return the removed PCB; pointing at highest priority (root)
-  Q[0] = Q[count - 1]; // move the last job to the front/root
-  count--; // decrement, since we are removing
-  to_remove->setState(ProcState::RUNNING); // state to running
-
-  int currentIndex = 0; // keep track of the current index, 0 is the root
-  int bigger_child = currentIndex; // holds location of the bigger child
-  
-  while(currentIndex < count) // runs until currentIndex exceeds count or no longer need to swap
-  {
-    bigger_child = getBiggerChild(currentIndex); // gets bigger child of parent
-    if(bigger_child != -1 && Q[bigger_child]->getPriority() > Q[currentIndex]->getPriority()) // the child is not -1 (error) and the bigger child is greater than the currentIndex
-    {
-      swap(Q[bigger_child], Q[currentIndex]); // swap both elements
-      currentIndex = bigger_child; // updating currentIndex
+    if (rsize == 0) {
+        return nullptr; // Return nullptr when the queue is empty
     }
-    else // no need to swap so break out of loop
-    {
-      break;
+
+    int priority = 0;
+    PCB *priorityreturn;
+    for (int i = 1; i < rsize; i++) {
+        if (arrqueue[priority]->getPriority() < arrqueue[i]->getPriority()) {
+            priority = i;
+        }
     }
-  } 
-  return to_remove; // returns PCB removed
+    priorityreturn = arrqueue[priority];
+    // Set the state of the removed process to RUNNING
+    priorityreturn->setState(ProcState::RUNNING);
+
+    for (int i = priority; i < rsize - 1; i++) {
+        arrqueue[i] = arrqueue[i + 1];
+    }
+    rsize--;
+
+    return priorityreturn;
 }
-
 /**
  * @brief Returns the number of elements in the queue.
  *
  * @return int: the number of PCBs in the queue
  */
 int ReadyQueue::size() {
-     return count;//return the size of the array
+    return rsize;
 }
 
 /**
  * @brief Display the PCBs in the queue.
  */
 void ReadyQueue::displayAll() {
-    if(count == 0)//if the queue is empty
-  {
-      cout << "Display Processes in ReadyQueue:" << endl;//set that as the statement to follow the test outputs (such as the last line of test1)
-  }
-  else // regular case - if there are elements inside the array
-  {
-    cout << "Display Processes in ReadyQueue:" << endl;//following the test outputs
-    for(int i=0; i<count; i++)//reads through the size of array
-      { 
-        Q[i]->display();//display the following statements from the pcb.h file
-      }
-    cout << endl;
+  for(int i = 0; i < rsize; i++){
+    arrqueue[i]->display();
   }
 }
-/**
-  @ brief Get the bigger child from the heap
-  */
-int ReadyQueue::getBiggerChild(int index) 
-{
-  // LC and RC are indexes
-  int LC = (2 * index) + 1;  //left child
-  int RC = (2 * index) + 2;  //right child 
-
-  if(LC >= count-1 && RC >= count-1) // both are beyond count-1 - error
-    return -1;
-  else if(LC >= count-1) // LC beyond count-1
-    return RC;
-  else if(RC >= count-1) // RC beyond count-1 
-    return LC;
-  else // regular case - both NOT beyond count-1 
-  {
-    if(Q[LC]->getPriority() > Q[RC]->getPriority()) // LC has higher priority
-	    return LC; // set it as left child
-    else
-	    return RC; // set it as right child
-  }
-}
-/*
-Swap function: set two values to switch values
-
-NOTE: WAS NOT USED TO RUN THE PROGRAM, BUT USED AS A TEST
-  
-*/
-void ReadyQueue::swapFunc(int x, int y) // original int x int y
-  {
-	// Swap only if both indexes are legal
-	if (x < count && y < count) {
-		PCB* temp = Q[x]; 
-		Q[x] = Q[y];
-		Q[y] = temp;
-	  }
-  }
-
-/*
-trickleup(): used to get the root of the heap array
-*/
-void ReadyQueue::trickleup()
-{
-  int j = count-1; // last job's location
-  int parent; // sucessor of child
-  
-  while(j > 0) // until j reaches 0
-  {
-    if(j%2 == 0) // is even
-      parent = ((j-2)/2); 
-    else  //is odd
-      parent = ((j-1)/2);
-
-    if(Q[j]->getPriority() > Q[parent]->getPriority()) // if child is bigger swap and update j
-    { 
-      swap(Q[j], Q[parent]);//call the swap function
-      j = parent; // update index
-    }
-    else  
-      return;
-  }  
-}
-
-#endif
